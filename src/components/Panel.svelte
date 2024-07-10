caad643dfbb28f414916df0b2f7da0ec300d6c5b
Kevin Doolaeghe <doolaeghe.kevin@gmail.com>
2024-07-10 18:16:51 +0200

    Added Panel component and variables to test

---
+++ src/components/Panel.svelte
@@ -0,0 +1,12 @@
+<script>
+    let count = 0;
+    let incCnt = () => count++;
+
+    let name = '';
+</script>
+
+<p>{count}</p>
+<button on:click={incCnt}>Click</button>
+
+<h1>Hello {name}</h1>
+<input bind:value={name} placeholder="name" />
